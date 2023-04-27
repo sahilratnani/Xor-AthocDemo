55cff40935fcb0b160823cdd200157d7db535dae
Sahil Ratnani <sahil.ratnani@xoriant.com>
2023-04-27 13:49:02 +0530

    Initial Commit

---
+++ Xor-AthocDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xor-AthocDemo
+//
+//  Created by Sahil Ratnani on 27/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
